f5412204a92899c02d2f9fb05ce86c47ec76ec9d
Joaquin Prego <joaquin.pregof@alumnos.unican.es>
2025-04-03 12:18:29 +0200

    Compilacion proyecto

---
+++ Practica5/src/main/java/es/unican/is2/src/Cuenta.java
@@ -0,0 +1,15 @@
+package es.unican.is2.src;
+
+public class Cuenta {
+	
+	private String numCuenta;
+	
+	public Cuenta(String numCuenta) {
+		this.numCuenta = numCuenta;
+	}
+	
+	public String getNumCuenta() {
+		return numCuenta;
+	}
+	
+}
---
+++ Practica5/src/main/java/es/unican/is2/src/saldoInsuficienteException.java
@@ -0,0 +1,9 @@
+package es.unican.is2.src;
+
+@SuppressWarnings("serial")
+public class saldoInsuficienteException extends RuntimeException {
+
+	public saldoInsuficienteException (String mensaje) {
+		super(mensaje);
+	}
+}
